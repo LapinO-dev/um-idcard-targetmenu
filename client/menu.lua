82ceadd77cf30963fea4197d0336f61b4cbb6aa1
LapinOrnithorynque <69824326+SienkoV3@users.noreply.github.com>
2024-04-27 00:54:27 +0200

    Update menu.lua
    Menu for ox_target

--- client/menu.lua
+++ client/menu.lua
@@ -1,25 +1,33 @@
-local identityMenu = {
-  id = 'identity_menu',
-  title = 'Select identity',
-  options = {},
-}
+local Options = {}
 
 for i = 1, #Config.MenuCards, 1 do
-  identityMenu.options[#identityMenu.options + 1] = {
-      title = Config.MenuCards[i].title,
-      icon = Config.MenuCards[i].icon,
-      serverEvent = 'um-idcard-npc:server:AddItemtoExport',
-      args = Config.MenuCards[i].args,
-      disabled = false,
-      onSelect = function()
-          identityMenu.options[i].disabled = true
-          lib.registerContext({identityMenu})
-      end,
-  }
+  if i == 1 then
+      Options[i] = {
+          title = Config.MenuCards[i].title,
+          icon = Config.MenuCards[i].icon,
+          args = Config.MenuCards[i].args,
+          serverEvent = 'um-idcard-npc:server:AddItemtoExport',
+      }
+    else
+      Options[i] = {
+        title = Config.MenuCards[i].title,
+        icon = Config.MenuCards[i].icon,
+        args = Config.MenuCards[i].args,
+        serverEvent = 'um-idcard-npc:server:AddItemtoExport',
+    }
+  end
 end
 
-lib.registerContext({identityMenu})
+lib.registerContext({
+  id = 'idmenu',
+  title = Config.Title.identite,
+  options = Options,
+})
 
+RegisterNetEvent('um-idcard-menu:menu')
+AddEventHandler('um-idcard-menu:menu', function()
+    lib.showContext('idmenu')
+end)
 RegisterNetEvent('um-idcard-npc:client:OxNotify', function(itemName)
   lib.notify({title = 'Identity Menu', description = 'Your identity has been approved: ' ..itemName, icon = 'id-card'})
 end)
