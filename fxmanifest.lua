05ce690cbdfbedc9a6bfcb6de2e2a3d62af9ab7b
LapinOrnithorynque <69824326+SienkoV3@users.noreply.github.com>
2024-04-27 00:57:03 +0200

    Update fxmanifest.lua
    fxmanifest update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-name 'um-idcard-menu'
-author 'uyuyorum {um}'
+name 'um-idcard-targetmenu'
+author 'uyuyorum {um} (fork of SienkoV3)'
 version '1.0.0'
 license 'GPL-3.0 license'
-repository 'https://github.com/alp1x/um-idcard-menu'
-description 'UM Identity Card Menu ox_lib'
+repository 'https://github.com/alp1x/um-idcard-targetmenu'
+description 'UM Identity Card Menu ox_lib with ox_target'
 
 shared_scripts {
 	'config.lua',
@@ -25,5 +25,6 @@ server_scripts {
 
 dependencies {
 	'um-idcard',
+	'ox_target',
 	'ox_lib'
 }
